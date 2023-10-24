2630c05880b84c9a3c0d55f22386d758b5af1517
Ben Madley <ben.madley@madetech.com>
2023-10-24 08:42:56 +0100

    Introduce European parrot.

--- Python/parrot.py
+++ Python/parrot.py
@@ -6,6 +6,9 @@ class ParrotType(Enum):
     AFRICAN = 2
     NORWEGIAN_BLUE = 3
 
+    @staticmethod
+    def european(number_of_coconuts, voltage, nailed):
+        return EuropeanParrot(ParrotType.EUROPEAN, number_of_coconuts, voltage, nailed)
 
 class Parrot:
 
@@ -17,7 +20,7 @@ class Parrot:
 
     def speed(self):
         if self._type == ParrotType.EUROPEAN:
-            return self._base_speed()
+            return ParrotType.european(self._number_of_coconuts, self._voltage, self._nailed).speed()
         if self._type == ParrotType.AFRICAN:
             return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
         if self._type == ParrotType.NORWEGIAN_BLUE:
@@ -30,7 +33,7 @@ class Parrot:
 
     def cry(self):
         if self._type == ParrotType.EUROPEAN:
-            return "Sqoork!"
+            return ParrotType.european(self._number_of_coconuts, self._voltage, self._nailed).cry()
         if self._type == ParrotType.AFRICAN:
             return "Sqaark!"
         if self._type == ParrotType.NORWEGIAN_BLUE:
@@ -49,3 +52,11 @@ class Parrot:
 
     def _base_speed(self):
         return 12.0
+
+
+class EuropeanParrot(Parrot):
+    def speed(self):
+        return self._base_speed()
+
+    def cry(self):
+        return "Sqoork!"
